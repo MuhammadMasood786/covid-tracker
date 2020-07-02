2e82dbff81ecfb550f422d748471bdc756f9ba01
Muhammad Masood <muhammadmasood673@gmail.com>
2020-07-02 17:02:02 +0500

    Updated Cases.js

--- src/Component/Cases.js
+++ src/Component/Cases.js
@@ -20,7 +20,7 @@ const Cases = () => {
                 <div className='infected borderBox'>
                     <p className='text-secondary'>Infected</p>
                     <h3 className='' style={{ color: 'orange' }}>
-                    <CountUp end={[globalState && globalState.confirmed && globalState.confirmed.value]} separator="," duration={2.5}></CountUp>
+                        <CountUp end={[globalState && globalState.confirmed && globalState.confirmed.value]} separator="," duration={2.5}></CountUp>
                     </h3>
                     <p className='text-secondary'>{new Date(globalState && globalState.lastUpdate && globalState.lastUpdate).toDateString()}</p>
                     <p className='situation'>Number of active cases of <br />COVID-19</p>
@@ -28,7 +28,7 @@ const Cases = () => {
                 <div className='infected borderBox1'>
                     <p className='text-secondary'>Recovered</p>
                     <h3 className='' style={{ color: 'green' }}>
-                    <CountUp end={[globalState && globalState.recovered && globalState.recovered.value]} separator="," duration={2.5}></CountUp>
+                        <CountUp end={[globalState && globalState.recovered && globalState.recovered.value]} separator="," duration={2.5}></CountUp>
                     </h3>
                     <p className='text-secondary'>{new Date(globalState && globalState.lastUpdate && globalState.lastUpdate).toDateString()}</p>
                     <p className='situation'>Number of active cases of <br />COVID-19</p>
@@ -36,7 +36,7 @@ const Cases = () => {
                 <div className='infected borderBox2'>
                     <p className='text-secondary' >Deaths</p>
                     <h3 className='' style={{ color: 'red' }}>
-                    <CountUp end={[globalState && globalState.deaths && globalState.deaths.value]} separator="," duration={2.5}></CountUp>
+                        <CountUp end={[globalState && globalState.deaths && globalState.deaths.value]} separator="," duration={2.5}></CountUp>
                     </h3>
                     <p className='text-secondary'>{new Date(globalState && globalState.lastUpdate && globalState.lastUpdate).toDateString()}</p>
                     <p className='situation'>Number of active cases of <br />COVID-19</p>
--- src/Component/Chart.js
+++ src/Component/Chart.js
@@ -1,72 +1,20 @@
 import React, { useEffect } from 'react';
-// import { Line, Bar } from 'react-chartjs-2';
+
 
 
 const Chart = () => {
-
-    // const [dailyData, setDailyData] = useState({});
 
     useEffect(() => {
         async function getData() {
             const response = await fetch('');
             const data = await response.json();
-            console.log(data);
-            
-            // setDailyData(data)
+            console.log(data); 
         }
         getData()
     }, [])
-
-    // const barChar = (
-    //     confirmed ? (
-    //         <Bar
-    //             data={{
-    //                 labels: ['Infected', 'Recovered', 'Deaths'],
-    //                 datasets: [
-    //                     {
-    //                         label: 'People',
-    //                         backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
-    //                         data: [confirmed.value, recovered.value, deaths.value],
-    //                     },
-    //                 ]
-    //                 ,
-    //             }}
-    //             options={{
-    //                 legend: { display: false },
-    //                 title: { display: true, text: `Current State in ${country}` }
-    //             }}
-    //         />
-    //     ) : null
-    // );
-
-    // const lineChart = (
-    //     dailyData[0] ? (
-    //         <Line
-    //             data={{
-    //                 labels: dailyData.map(({ date }) => date),
-    //                 datasets: [{
-    //                     data: dailyData.map((data) => data.confirmed),
-    //                     label: 'Infected',
-    //                     borderColor: '#3333ff',
-    //                     fill: true,
-    //                 }, {
-    //                     data: dailyData.map((data) => data.deaths),
-    //                     label: 'Deaths',
-    //                     borderColor: 'red',
-    //                     backgroundColor: 'rgba(255, 0, 0, 0.5)',
-    //                     fill: true,
-    //                 },
-    //                 ],
-    //             }}
-    //         />
-    //     ):null
-    // );
-
-
-
     return (
         <div>
-            {/* {country ? barChar : lineChart} */}
+            
         </div>
     );
 }
