435150662c41c2de37990cae0b44b98f8a3f59b7
Muhammad Masood <muhammadmasood673@gmail.com>
2020-07-05 21:29:19 +0500

    Changes & Delete
    Changes of some file and delete chart.js because some error occur

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {  } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './Component/Header';
 import Cases from './Component/Cases';
 import State from './Component/State';
-import Chart from './Component/Chart';
 
 function App() {
 
@@ -12,7 +11,6 @@ function App() {
       <div className="justify-content-center spacing mt-5">
         <Header />
         <Cases/>
-        <Chart/>
         <State/>
       </div>
     </div>
